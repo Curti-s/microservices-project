274e51512ac9db6626252984ce665b5204f651d0
Curti.s_ <matthewscurtis.dev81@gmail.com>
2019-04-20 11:44:38 +0300

    [:whale: Add Dockerfile and docker-compose]

--- flask_users/__init__.py
+++ flask_users/__init__.py
@@ -1,10 +1,16 @@
+import os
+import sys
+
 from flask import Flask, jsonify
+
 
 # initialize the app
 app = Flask(__name__)
 
 # set config
-app.config.from_object('flask_users.config.DevelopmentConfig')
+app_settings = os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+print(app.config,file=sys.stderr)
 
 @app.route('/ping', methods=('GET',))
 def ping_pong():
