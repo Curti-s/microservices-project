4a924295c1d301fb67677ffa6758a17057bba689
Curti.s_ <matthewscurtis.dev81@gmail.com>
2019-05-24 02:15:32 +0300

    [:sparkles: Add store and combined the reducers]

--- flask_client/src/index.js
+++ flask_client/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router } from 'react-router-dom';
-
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store, history } from "./redux/store";
 
 ReactDOM.render(
+  <Provider>
     <Router>
-        <App />
-    </Router>,
-    document.getElementById('root')
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
